96ba425df65c85005495caa895f945787697fc70
FarhanSE <farhanbhn2222@gmail.com>
2021-07-28 18:51:35 +0500

    In this commit: I learn to follow link and scrape year, population with country name

--- w_metter/spiders/countries.py
+++ w_metter/spiders/countries.py
@@ -14,13 +14,15 @@ class CountriesSpider(scrapy.Spider):
             # ful_url = f"https://www.worldometers.info{country_link}"
             # ful_url = response.urljoin(country_link)
 
-            yield response.follow(country_link, callback=self.country_data)
+            yield response.follow(country_link, callback=self.country_data, meta = {"country_name":country_name})
     def country_data(slef, response):
+        name = response.request.meta["country_name"]
         row = response.xpath("(//table[@class='table table-striped table-bordered table-hover table-condensed table-list'])[1]/tbody/tr")
         for r in row:
             year = r.xpath(".//td[1]/text()").get()
             population = r.xpath(".//td[2]/text()").get()
             yield {
+                "Country name":name,
                 "Year": year,
                 "Population": population
             }
